311b8917009af6dd90ada853b5352921fde0b74d
VarionDrakon <52794446+VarionDrakon@users.noreply.github.com>
2022-12-09 21:47:32 +0300

    PublicherFilex

---
+++ SourceCode/LaucnherYouTube/MainWindow.xaml.cs
@@ -0,0 +1,135 @@
+﻿using LaucnherYouTube.ChildWindow;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Threading;
+using System.Xml;
+
+namespace LaucnherYouTube
+{
+    public partial class MainWindow : Window
+    {
+        private string xmlFile = "Assets/version.xml";
+        private string _stateLocateVersionXML;
+        private string _stateServerVersionXML;
+        private int? idProcessApp = null;
+        private bool appIsStarting = false;
+        private DispatcherTimer dispatcherTimer;
+        public MainWindow()
+        {
+            ServerXMLDownload();
+            ServerVersionXML();
+
+            InitializeComponent();
+
+            LocateVersionXML();
+            TextBlock();
+            CheckAppLaunchTimer();
+        }
+        private void LocateVersionXML()
+        {
+            XmlReader reader = XmlReader.Create(xmlFile);
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.Name == "Main")
+                    {
+                        reader.ReadToFollowing("version");
+                        _stateLocateVersionXML = reader.ReadElementContentAsString();
+                    }
+                }
+            }
+        }
+        private static WebClient client;
+        public void ServerXMLDownload()
+        {
+            if (client == null || !client.IsBusy)
+            {
+                client = new WebClient();
+                client.DownloadFileCompleted += CompleteDownloadVersionXMLServer;
+                client.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/VarionDrakon/LauncherGame/main/versionServer.xml"), "Assets/versionServer.xml");
+            }
+            if (client != null)
+            {
+                MessageBox.Show("Debug");
+            }
+        }
+        private void CompleteDownloadVersionXMLServer(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null || e.Cancelled)
+            {
+                MessageBox.Show("Download error: " + e.Error);
+            }
+            else
+            {
+                MessageBox.Show("Download complete!");
+            }
+        }
+        public void ServerVersionXML()
+        {
+            XmlTextReader reader = new XmlTextReader("Assets/versionServer.xml");
+            while (reader.Read())
+            {
+                if (reader.NodeType == XmlNodeType.Element)
+                {
+                    if (reader.Name == "Main")
+                    {
+                        reader.ReadToFollowing("version");
+                        _stateServerVersionXML = reader.ReadElementContentAsString();
+                    }
+                }
+            }
+        }
+        private void TextBlock()
+        {
+            _textCurrentVersion.Text += _stateLocateVersionXML;
+            _textServerVersion.Text += _stateServerVersionXML;
+        }
+        private void ButtonUpdateDialogWindow(object sender, RoutedEventArgs rea)
+        {
+            UpdateWindow updateWindow = new UpdateWindow();
+            updateWindow.ShowDialog();
+        }
+        private void ButtonLaunchGame(object sender, RoutedEventArgs rea)
+        {
+            ProcessStartInfo procInfoLaunchGame = new ProcessStartInfo();
+            procInfoLaunchGame.FileName = @"Game\Steampunk Edge - IcePunk.exe";
+            Process processApp = new Process();
+            processApp.StartInfo = procInfoLaunchGame;
+            processApp.Start();
+            idProcessApp = processApp.Id;
+        }
+        private void CheckAppLaunchTimer()
+        {
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(SingleAppTimerCheckMethod);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 5);
+            dispatcherTimer.Start();
+        }
+        public void SingleAppTimerCheckMethod(object sender, EventArgs ea)
+        {
+            Process[] processedUsers = Process.GetProcesses();
+            foreach (Process allprocessed in processedUsers)
+            {
+                if (allprocessed.Id == idProcessApp)
+                {
+                    appIsStarting = true;
+                    break;
+                }
+                else if (allprocessed.Id != idProcessApp) appIsStarting = false;
+            }
+            if (appIsStarting == false)
+            {
+                AppState.Text = "App is not starting!";
+            }
+            else
+            {
+                AppState.Text = "App is starting!";
+            }
+        }
+    }
+}
