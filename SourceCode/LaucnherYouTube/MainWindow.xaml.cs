59739ba2767e9e9f6c30a2513937851428102a71
VarionDrakon <52794446+VarionDrakon@users.noreply.github.com>
2022-12-16 19:16:40 +0300

    UpdateFiles
    Update Launcher up 1.0.1

--- SourceCode/LaucnherYouTube/MainWindow.xaml.cs
+++ SourceCode/LaucnherYouTube/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
+using System.IO.Compression;
 using System.Net;
 using System.Windows;
 using System.Windows.Threading;
@@ -12,11 +13,14 @@ namespace LaucnherYouTube
 {
     public partial class MainWindow : Window
     {
-        private string xmlFile = "Assets/version.xml";
+        private readonly string zipPath = @".\ChacheDownloadGame.zip";
+        private readonly string appTemlPath = "tempDirectoryUnzip";
+        private readonly string xmlFile = "Assets/version.xml";
         private string _stateLocateVersionXML;
         private string _stateServerVersionXML;
         private int? idProcessApp = null;
         private bool appIsStarting = false;
+        private bool isStartUnzipUpdateFileApp = true;
         private DispatcherTimer dispatcherTimer;
         public MainWindow()
         {
@@ -26,9 +30,12 @@ namespace LaucnherYouTube
             InitializeComponent();
 
             LocateVersionXML();
-            TextBlock();
             CheckAppLaunchTimer();
+            //ServerDownloadChacheGame();
+            ArgumentsUIValue();
         }
+        #region XMLREAD
+        private static WebClient client;
         private void LocateVersionXML()
         {
             XmlReader reader = XmlReader.Create(xmlFile);
@@ -44,18 +51,25 @@ namespace LaucnherYouTube
                 }
             }
         }
-        private static WebClient client;
         public void ServerXMLDownload()
         {
             if (client == null || !client.IsBusy)
             {
+                try
+                {
                 client = new WebClient();
                 client.DownloadFileCompleted += CompleteDownloadVersionXMLServer;
                 client.DownloadFileAsync(new Uri("https://raw.githubusercontent.com/VarionDrakon/LauncherGame/main/versionServer.xml"), "Assets/versionServer.xml");
+                }
+                catch
+                {
+                    ServerConnecting.Text = "Server offline!";
+                }
+
             }
             if (client != null)
             {
-                MessageBox.Show("Debug");
+                ServerConnecting.Text = "Server online!";
             }
         }
         private void CompleteDownloadVersionXMLServer(object sender, AsyncCompletedEventArgs e)
@@ -84,8 +98,11 @@ namespace LaucnherYouTube
                 }
             }
         }
-        private void TextBlock()
+        #endregion
+        #region UIFUNC
+        private void ArgumentsUIValue()
         {
+            ProgressBarExtractFile.Minimum = 0;
             _textCurrentVersion.Text += _stateLocateVersionXML;
             _textServerVersion.Text += _stateServerVersionXML;
         }
@@ -103,6 +120,14 @@ namespace LaucnherYouTube
             processApp.Start();
             idProcessApp = processApp.Id;
         }
+        private void ProgressDownloadServerGame(object sender, DownloadProgressChangedEventArgs dpcea)
+        {
+            ProgressBarExtractFile.Maximum = 100;
+            DownloadAppState.Text = "Download: " + dpcea.BytesReceived + " of " + dpcea.TotalBytesToReceive + " . Process: " + dpcea.ProgressPercentage + "%";
+            ProgressBarExtractFile.Value = dpcea.ProgressPercentage;
+        }
+        #endregion
+        #region BACKGROUNDFUNC
         private void CheckAppLaunchTimer()
         {
             dispatcherTimer = new DispatcherTimer();
@@ -131,5 +156,76 @@ namespace LaucnherYouTube
                 AppState.Text = "App is starting!";
             }
         }
+        #endregion
+        #region UPDATEFUNC
+        public void ServerDownloadChacheGame()
+        {
+            if (client == null || !client.IsBusy)
+            {
+                client = new WebClient();
+                client.DownloadFileCompleted += CompleteDownloadChacheGame;
+                MessageBox.Show("Start Download");
+                client.DownloadFileAsync(new Uri("https://drive.google.com/uc?export=download&confirm=no_antivirus&id=10g0Vd_GWyt7VwF392q77NVBNibfGzQLi"), zipPath);
+                client.DownloadProgressChanged += new DownloadProgressChangedEventHandler(ProgressDownloadServerGame);
+            }
+            if (client != null)
+            {
+                MessageBox.Show("Debug");
+            }
+        }
+        private void CompleteDownloadChacheGame(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Error != null || e.Cancelled)
+            {
+                DownloadAppState.Text = "Download error: " + e.Error;
+            }
+            else
+            {
+                ProgressBarExtractFile.Value = 0;
+                ZipArchive zipFileServer = ZipFile.OpenRead(zipPath);
+                int zipFilesCount = zipFileServer.Entries.Count;
+                ProgressBarExtractFile.Maximum = zipFilesCount;
+
+                foreach (var zip in zipFileServer.Entries)
+                {
+                    if (isStartUnzipUpdateFileApp == true)
+                    {
+                        zip.Archive.ExtractToDirectory(appTemlPath);
+                        isStartUnzipUpdateFileApp = false;
+                        break;
+                    }
+                }
+                foreach (string dgfse in Directory.GetFileSystemEntries(appTemlPath + "/Game"))
+                {
+                    FileAttributes attributes = File.GetAttributes(dgfse);
+                    DirectoryInfo dirInf = new DirectoryInfo(dgfse);
+                    FileInfo fileInf = new FileInfo(dgfse);
+                    if (Directory.Exists(@"Game/") == false)
+                    {
+                        Directory.CreateDirectory("Game");
+                    }
+                    if ((attributes & FileAttributes.Directory) == FileAttributes.Directory)
+                    {
+                        if (Directory.Exists(@"Game/" + dirInf.Name) == true)
+                        {
+                            Directory.Delete(@"Game/" + dirInf.Name, true);
+                        }
+                        dirInf.MoveTo(@"Game/" + dirInf.Name);
+                    }
+                    else if ((attributes & FileAttributes.Directory) != FileAttributes.Directory)
+                    {
+                        if (File.Exists(@"Game/" + fileInf.Name) == true)
+                        {
+                            File.Delete(@"Game/" + fileInf.Name);
+                        }
+                        fileInf.MoveTo(@"Game/" + fileInf.Name);
+                    }
+                }
+                ProgressBarExtractFile.Value = zipFilesCount;
+                DownloadAppState.Text = "Game installed!";
+            }
+        }
+        #endregion
     }
 }
+
